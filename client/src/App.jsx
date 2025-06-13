7e16570146ca2bb35b6a48da8a585aec25d2579a
JUDHI3134 <judhistirbehera456@gmail.com>
2025-06-13 15:34:09 +0530

    folder structer with create route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import Seatlayout from './pages/Seatlayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
+import Footer from './components/Footer'
+import {Toaster} from "react-hot-toast"
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith("/admin")
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<Seatlayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
   )
 }
 
